78108588d667e1be93afa9d0afae2e145f1e29f5
miqueiasbelfort <kawasousa1554@gmail.com>
2022-03-07 18:42:41 -0300

    sistema de respostas

---
+++ db.js
@@ -0,0 +1,55 @@
+const ask1 = {
+    id: 1,
+    ask: "Onde fica o Rio?",
+    answer1: "Brazil",
+    answer2: "USA",
+    answer3: "UK",
+    answer4: "Mexico",
+    right: "Brazil"
+}
+const ask2 = {
+    id: 2,
+    ask: "Onde Fica Tokyo?",
+    answer1: "China",
+    answer2: "Japão",
+    answer3: "Argentina",
+    answer4: "Portugal",
+    right: "Japão"
+}
+const ask3 = {
+    id: 3,
+    ask: "Quanto é 1 + 2?",
+    answer1: "2",
+    answer2: "5",
+    answer3: "3",
+    answer4: "1",
+    right: "3"
+}
+const ask4 = {
+    id: 4,
+    ask: "Qual pais desses esta em guerra contra a Russia?",
+    answer1: "Ukrania",
+    answer2: "USA",
+    answer3: "Turkia",
+    answer4: "Venezuela",
+    right: "Ukrania"
+}
+const ask5 = {
+    id: 5,
+    ask: "Quanto é 5 + 3?",
+    answer1: "8",
+    answer2: "5",
+    answer3: "10",
+    answer4: "7",
+    right: "8"
+}
+
+const ask6 = {
+    id: 6,
+    ask: "Em que ano começou a 2º Guerra Mundial?",
+    answer1: "1945",
+    answer2: "1939",
+    answer3: "1891",
+    answer4: "1925",
+    right: "1939"
+}
--- script.js
+++ script.js
@@ -1,87 +1,45 @@
-const ask1 = {
-    id: 1,
-    ask: "Onde fica o Rio?",
-    answer1: "Brazil",
-    answer2: "USA",
-    answer3: "UK",
-    answer4: "Mexico",
-    right: "Brazil"
-}
-const ask2 = {
-    id: 2,
-    ask: "Onde Fica Tokyo?",
-    answer1: "China",
-    answer2: "Japão",
-    answer3: "Argentina",
-    answer4: "Portugal",
-    right: "Japão"
-}
-const ask3 = {
-    id: 3,
-    ask: "Quanto é 1 + 2?",
-    answer1: "2",
-    answer2: "5",
-    answer3: "3",
-    answer4: "1",
-    right: "3"
-}
-const ask4 = {
-    id: 4,
-    ask: "Qual pais desses esta em guerra contra a Russia?",
-    answer1: "Ukrania",
-    answer2: "USA",
-    answer3: "Turkia",
-    answer4: "Venezuela",
-    right: "Ukrania"
-}
-const ask5 = {
-    id: 5,
-    ask: "Quanto é 5 + 3?",
-    answer1: "8",
-    answer2: "5",
-    answer3: "10",
-    answer4: "7",
-    right: "8"
-}
 
-const ask6 = {
-    id: 6,
-    ask: "Em que ano começou a 2º Guerra Mundial?",
-    answer1: "1945",
-    answer2: "1939",
-    answer3: "1891",
-    answer4: "1925",
-    right: "1939"
-}
-
-const data = [ask1,ask2,ask3,ask4,ask5,ask6]
-const answerData = [ask1.right, ask2.right, ask3.right, ask4.right, ask5.right, ask6.right]
+const data = [ask1,ask2,ask3,ask4,ask5,ask6] //array com os objetos
+//array com as respostas dos objetos
+const answerData = [ask1.right, ask2.right, ask3.right, ask4.right, ask5.right, ask6.right] 
 
 const next = document.getElementById("btnNext")
 const quiz = document.querySelector(".quiz")
 const answersNumbers = document.getElementById("answers")
 const asksNumbers = document.getElementById("asks")
+const quizContainer = document.querySelector(".quizContainer")
+const textIntroducion = document.getElementById("textIntroducion")
 
 
-let numId = 0
-answersNumbers.innerHTML = numId
-asksNumbers.innerHTML = data.length
+let numId = 0 
+answersNumbers.innerHTML = numId // numero de respostas = 0
+asksNumbers.innerHTML = data.length // quantidade de perguntas
 let rightAnswers = 0
 
 
 function percorerBtn(answer, isTrue){
-    answer.forEach(btns => {
-        if (btns.textContent != isTrue){
-            btns.style.background = "red"
+    answer.forEach(btns => { // percorendo as respostas
+        
+        if (btns.textContent != isTrue){ //as respostas que forem diferentes da repostas corretas
+            btns.style.background = "red" // ficarão vermelhas
         } else {
-            btns.style.background = "green"
+            btns.style.background = "green" // a corretas ficara verder
         }
     })
+    
 }
+
+function reloadPage(){
+    window.location.reload() //função para reiniciar a pagina
+}
+
 
 next.addEventListener("click", function(){
    numId++
-   
+   next.innerHTML = "Próximo"
+   textIntroducion.innerHTML = "Quando marcar a sua resposta clique em próximo para ir a outra pergunta!"
+
+   //percorrndo o array com as perguntas e respostas
    data.forEach(e => {
 
         if (e.id == numId){ //se o id for igual ao numId 
@@ -97,32 +55,38 @@ next.addEventListener("click", function(){
             `
 
             const answer = document.querySelectorAll(".answer")
-            answer.forEach(button => {
+
+            answer.forEach(button => { //Percorendo as respostas 
+                //Add um evento de click quando clicar em uma delas
                 button.addEventListener("click", (elementButton) => {
-                    if(elementButton.target.textContent == e.right){
-                        rightAnswers++
+
+                    if(elementButton.target.textContent == e.right){ //Se o conteudo dela for igual a resposta correta
+                        rightAnswers++ //Somar mais pontos
                         button.style.background = "green"
+                        
                     }else {
+                        //callback
                         percorerBtn(answer, e.right)
                     }
-
                 })
             })
 
         } else if (numId > data.length){ // se o numId for maior que o tamanha de data
+            
             quiz.innerHTML = `
-                Você acertou ${rightAnswers} de ${data.length}
-                Porcentagem de ${Math.round(rightAnswers * 10/data.length * 10)}%
-                `   
+                Você acertou ${rightAnswers} de ${data.length} | 
+                Porcentagem de ${Math.round(rightAnswers * 10 / data.length * 10)}%
+            `
+            textIntroducion.innerHTML = " "
+            next.style.display = "none"
         }
     })
 
     answersNumbers.innerHTML = numId
     if (numId > data.length){
         answersNumbers.innerHTML = 0
+        quizContainer.innerHTML += `
+            <button class="btnNext" onclick="reloadPage()" >Reiniciar</button>
+        `
     }
 })
-
-sfaknsjfnaskjfn
-asdnfjasdnfokjasnd
-nsajkdnfgsajodvnos
